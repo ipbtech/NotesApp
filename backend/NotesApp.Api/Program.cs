31256e3fc97e3a48b3060fc848536ea5dd471013
Петров Илья Вадимович <ipb.tech@yandex.ru>
2025-01-08 00:36:06 +0300

    add docker pgsql

--- backend/NotesApp.Api/Program.cs
+++ backend/NotesApp.Api/Program.cs
@@ -1,10 +1,13 @@
+using NotesApp.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDataAccess(builder.Configuration);
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
